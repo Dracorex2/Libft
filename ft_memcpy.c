4b7b3da10623b8b79b2ca831a0e2562960f3fedd
Dracorex2 <lucas.m3166@gmail.com>
2024-11-05 14:00:38 +0100

    auto update

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: lucmansa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 14:23:45 by lucmansa          #+#    #+#             */
-/*   Updated: 2024/11/05 13:59:28 by lucmansa         ###   ########.fr       */
+/*   Updated: 2024/11/05 14:00:36 by lucmansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	char	*d;
 	char	*s;
 	
-	if (n == 0)
-		return (NULL);
 	d = (char *)dest;
 	s = (char *)src;
 	while (n--)
