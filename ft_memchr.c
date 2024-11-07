21f84f494ae803fce36a9668d894fb7831fe493c
Dracorex2 <lucas.m3166@gmail.com>
2024-11-07 12:51:30 +0100

    fix chr

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: lucmansa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:01:58 by lucmansa          #+#    #+#             */
-/*   Updated: 2024/11/05 17:07:52 by lucmansa         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:49:02 by lucmansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	char		*shr;
 
 	i = 0;
+	c %= 256;
 	shr = (char *)s;
 	while (i < n)
 	{
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: lucmansa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 15:18:06 by lucmansa          #+#    #+#             */
-/*   Updated: 2024/11/06 12:09:44 by lucmansa         ###   ########.fr       */
+/*   Updated: 2024/11/07 12:49:05 by lucmansa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@ char	*ft_strrchr(const char *s, int c)
 	int	i;
 
 	i = 0;
+	c %= 256;
 	while (s[i] != 0)
 		i++;
 	while (s[i] != c)
